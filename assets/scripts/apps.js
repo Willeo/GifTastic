706af98427c26c37ec2041153c8373caab6f6fb4
Willeo <willeo.smith@outlook.com>
2018-07-14 16:10:57 -0500

    Code is working, need to style the project

--- assets/scripts/apps.js
+++ assets/scripts/apps.js
@@ -1,55 +1,123 @@
+var carsArr = ["Lamborghini", "Ferrari", "Audi"];
 
+//Functions were breaking...notating e-v-e-r-y-t-h-i-n-g; moved document.ready to bottom.
+// renderButtons will display the car buttons for all cars within the carsArr array.
+function renderButtons() {
+  // Empty the buttons panel before redrawing it
+  $("#buttonPanel").empty();
 
-/* var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=star+wars&b0JAGUJDjol9HySDb1qMsu16MrDHEEL3&limit=10");
-xhr.done(function(data) { console.log("success got data", data); });
+  // Loop through the array of cars
+  for (var i = 0; i < carsArr.length; i++) {
+    // Dynamicaly generate a button for each car in the array
+    var button = $("<button>");
+    button.addClass("carButton");
+    button.attr("data-car", carsArr[i]);
+    button.text(carsArr[i]);
 
-static image data access
-static image url-> data.images.fixed_height_still.url */
+    // Add the button to the HTML
+    $("#buttonPanel").append(button);
+  }
+}
+//Event handlers
 
-$(document).ready(function(){
+// An event handler for the user form to add additional cars to the array
+$("#add-car").on("click", function(event) {
+  event.preventDefault();
 
-    //This function will do a search call to the Giphy API and will create the still image <img> tags
-    //Recommend including the onClick function here to consolidate still/animated
-    
-    $('button').on('click', function() {
-    
-        var cars = $(this).data('name');
-        var giphyQ = "https://api.giphy.com/v1/gifs/search?q=" + cars + "&api_key=b0JAGUJDjol9HySDb1qMsu16MrDHEEL3&limit=10";
-    
-        $.ajax({
-            url: giphyQ,
-            method: 'GET'
-        })
-            .done (function (response){
-              console.log(response)
-            var apiResponse = response.data;
+  // Get the input from the textbox
+  var car = $("#car-input")
+    .val()
+    .trim();
 
-            for(var i = 0; i < results.length; i++){
-                var divTag = $('<div/>');
-                
-    // create var for jQuery tag creation  
-      
-              var pTag = $('<p/>');
-              var imgTag = $('<img/>');
-              p.text(results[i].rating);
-              
-              imgTag.addClass('something');//whatever class you want the images to use
-              imgTag.attr('src', apiResponse[i].images.fixed_height.url);
-              imgTag.attr('data-still', apiResponse[i].images.fixed_height_still.url);// data attributes can be used for the onClick function
-              imgTag.attr('data-animated', apiResponse[i].images.fixed_height.url);
-              
-              
-              
-            }
-          
-          
-          
-          
-          })
-    
-    /*user submission and parse function will go here
-    *
-    *
-    */
-    });
-    });
+  // The car from the textbox is then added to our carsArr array
+  carsArr.push(car);
+  $("#car-input").val("");
+
+  // Redraw the car buttons
+  renderButtons();
+});
+
+// fetchcargifs will fetch car gifs with the Giphy API
+function fetchcargifs() {
+  // Get the car name from the button clicked
+  var carName = $(this).attr("data-car");
+  var carStr = carName.split(" ").join("+");
+
+  // make the giphy URL
+  var queryURL =
+    "https://api.giphy.com/v1/gifs/search?q=" +
+    carStr +
+    "&limit=10&api_key=b0JAGUJDjol9HySDb1qMsu16MrDHEEL3";
+
+  // AJAX giphy API call
+  $.ajax({
+    method: "GET",
+    url: queryURL
+  }).done(function(result) {
+    // Get the results array
+    var dataArray = result.data;
+    // Create and display div elements for each of the returned gifs
+    $("#gifPanel").empty();
+    for (var i = 0; i < dataArray.length; i++) {
+      var newDiv = $("<div>");
+      newDiv.addClass("cargif");
+      var newRating = $("<h2>").html("Rating: " + dataArray[i].rating);
+      newDiv.append(newRating);
+      var newImg = $("<img>");
+      newImg.attr("src", dataArray[i].images.fixed_height_still.url);
+      newImg.attr("data-still", dataArray[i].images.fixed_height_still.url);
+      newImg.attr("data-animate", dataArray[i].images.fixed_height.url);
+      newImg.attr("data-state", "still");
+      newDiv.append(newImg);
+
+      // Append the new gifs to the gifPanel
+      $("#gifPanel").append(newDiv);
+    }
+  });
+}
+// animatecargif will animate a still gif and stop a moving gif
+function animatecargif() {
+  // The image state will be either "still" or "animated", function will swap states onClick via handler
+  var state = $(this)
+    .find("img")
+    .attr("data-state");
+
+  // Make the gif either animated or still depending on the "data-state" value
+  if (state === "still") {
+    $(this)
+      .find("img")
+      .attr(
+        "src",
+        $(this)
+          .find("img")
+          .attr("data-animate")
+      );
+    $(this)
+      .find("img")
+      .attr("data-state", "animate");
+  } else {
+    $(this)
+      .find("img")
+      .attr(
+        "src",
+        $(this)
+          .find("img")
+          .attr("data-still")
+      );
+    $(this)
+      .find("img")
+      .attr("data-state", "still");
+  }
+}
+
+// relocated ready function as it was causing issues initally
+//
+$(document).ready(function() {
+  renderButtons();
+});
+
+// An event handler for the car buttons to fetch appropriate gifs
+$(document).on("click", ".carButton", fetchcargifs);
+
+// Add an event handler for the car gifs to make the image animate and stop
+$(document).on("click", ".cargif", animatecargif);
